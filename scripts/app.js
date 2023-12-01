bb1c2c29c410893739b0c7a5c578f129603a4e13
Caleb Sylvia <caleb_sylvia@yahoo.com>
2023-12-01 15:52:50 -0800

    key

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
 import { weatherKey, mapKey } from "./keys";
 
-fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API key}`)
+fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${weatherKey}`);
 
