bb9762f92ed0e368ab5c6e8fdfc085236daf3e5c
Caleb Sylvia <caleb_sylvia@yahoo.com>
2023-12-02 18:37:44 -0800

    third commit (notes in doc.txt)

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,4 @@ import { weatherKey, mapKey } from "./keys";
 
 fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${weatherKey}`);
 
+let favBtn = document.getElementById('favBtn');
