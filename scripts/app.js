87482f840ee2aa712af6764a45f4557c1210610c
Caleb Sylvia <caleb_sylvia@yahoo.com>
2023-12-03 21:21:52 -0800

    fourth commit (notes in doc.txt)

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,47 @@
-import { weatherKey, mapKey } from "./keys";
+//import { weatherKey, mapKey } from "./keys";
 
-fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${weatherKey}`);
-
+//Current Weather variables
 let favBtn = document.getElementById('favBtn');
+let cityName = document.getElementById('cityName');
+let currentTemp = document.getElementById('currentTemp');
+let highLow = document.getElementById('highLow');
+let mainIcon = document.getElementById('mainIcon');
+
+//Current Weather extra variables
+let precText = document.getElementById('precText');
+let windText = document.getElementById('windText');
+let humiText = document.getElementById('humiText');
+let latText = document.getElementById('latText');
+let lonText = document.getElementById('lonText');
+let riseText = document.getElementById('riseText');
+let setText = document.getElementById('setText');
+
+// fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=${part}&appid=${weatherKey}`);
+
+
+// Get user location
+function userLocation() {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(userPos);
+    }else{
+        console.log('Not supported');
+    }
+}
+
+function userPos(position){
+    latText.innerText = `Lat: ${Math.round(position.coords.latitude * 1000) / 1000}`;
+    lonText.innerText = `Lon: ${Math.round(position.coords.longitude * 1000) / 1000}`;
+}
+window.onload = userLocation();
+
+//Weather Function
+
+
+navigator.geolocation.getCurrentPosition(weatherCall);
+
+function weatherCall(position){
+//fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=${weatherKey}`)
+   // .then
+
+}
+weatherCall();
---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+// import { weatherKey } from "./keys";
+
+// Display days variables
+let day1 = document.getElementById("day1");
+let day2 = document.getElementById("day2");
+let day3 = document.getElementById("day3");
+let day4 = document.getElementById("day4");
+let day5 = document.getElementById("day5");
+
+// Display Icons variables
+let dayIcon1 = document.getElementById("dayIcon1");
+let dayIcon2 = document.getElementById("dayIcon2");
+let dayIcon3 = document.getElementById("dayIcon3");
+let dayIcon4 = document.getElementById("dayIcon4");
+let dayIcon5 = document.getElementById("dayIcon5");
+
+//High and Low variables
+let day1HL = document.getElementById("day1HL");
+let day2HL = document.getElementById("day2HL");
+let day3HL = document.getElementById("day3HL");
+let day4HL = document.getElementById("day4HL");
+let day5HL = document.getElementById("day5HL");
+
+//fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}`);
